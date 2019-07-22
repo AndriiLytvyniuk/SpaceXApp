822d262013aa63553b3e88ba8dc9691ca0b6ca9e
alytvyniuk <andriilytvyniuk@gmail.com>
2019-07-22 21:57:01 +0300

    fixed build

--- data/src/main/java/alytvyniuk/com/data/repository/LaunchesRetrofitClient.kt
+++ data/src/main/java/alytvyniuk/com/data/repository/LaunchesRetrofitClient.kt
@@ -4,21 +4,21 @@ import alytvyniuk.com.model.LaunchData
 import alytvyniuk.com.model.LaunchesRepository
 import io.reactivex.Single
 
-internal class LaunchesRetrofitClient(val launchesRetrofitApi: LaunchesRetrofitApi): LaunchesRepository {
-    override fun getAllLaunches(): Single<Result<List<LaunchData>>> {
-            return Single.fromCallable {
-                launchesRetrofitApi.getAllLaunches().map {
-                    if(it.isSuccessful) {
-                        val l = it.body()?.map { responseModel ->
-                            LaunchData(responseModel.flight_number)
-                        }
-                        Result.success(l)
-                    }
-                }
-            }
-    }
-
+internal class LaunchesRetrofitClient(private val launchesRetrofitApi: LaunchesRetrofitApi) : LaunchesRepository {
     override fun getLaunchesInRange(start: Int, count: Int): Single<Result<List<LaunchData>>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllLaunches(): Single<Result<List<LaunchData>>> {
+        return launchesRetrofitApi.getAllLaunches()
+            .map { t ->
+                if (t.isSuccessful) {
+                    val l = t.body()?.map { responseModel ->
+                        LaunchData(responseModel.flight_number)
+                    } ?: emptyList()
+                    Result.success(l)
+                }
+                Result.failure(Throwable(t.message()))
+            }
     }
 }
